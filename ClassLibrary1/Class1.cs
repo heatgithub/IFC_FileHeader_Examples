7c4ac6d9383d7bacd8da5eab9c0d6c5b180b48f6
Henrik Ericson <henrik@hericson.se>
2019-01-16 20:06:59 +0100

    First commit

---
+++ ClassLibrary1/Class1.cs
@@ -0,0 +1,24 @@
+﻿using Autodesk.AutoCAD.Runtime;
+using GeometryGym.Ifc;
+using System.IO;
+
+namespace ClassLibrary1
+{
+    public class Class1
+    {
+		[CommandMethod("ifcout")]
+		public void Main()
+		{
+			DatabaseIfc db = new DatabaseIfc(ModelView.Ifc2x3Coordination);
+
+			IfcSite site = new IfcSite(db, "Default Site");
+			IfcProject project = new IfcProject(site, "Default Project", IfcUnitAssignment.Length.Metre) { };
+			IfcBuilding building = new IfcBuilding(site, "Default Building");
+			IfcBuildingStorey buildingStorey = new IfcBuildingStorey(building, "SitePlan", 0.0);
+
+			string currentDir = Path.GetDirectoryName(System.Environment.CurrentDirectory);
+			db.WriteFile(Path.Combine(currentDir, "ClassLibrary1.ifc"));
+		}
+
+	}
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,22 @@
+﻿using GeometryGym.Ifc;
+using System.IO;
+
+namespace ConsoleApp1
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			DatabaseIfc db = new DatabaseIfc(ModelView.Ifc2x3Coordination);
+
+			IfcSite site = new IfcSite(db, "Default Site");
+			IfcProject project = new IfcProject(site, "Default Project", IfcUnitAssignment.Length.Metre) { };
+			IfcBuilding building = new IfcBuilding(site, "Default Building");
+			IfcBuildingStorey buildingStorey = new IfcBuildingStorey(building, "SitePlan", 0.0);
+
+			DirectoryInfo di = Directory.GetParent(Path.GetDirectoryName(System.Reflection.Assembly.GetEntryAssembly().Location));
+			di = Directory.GetParent(di.FullName);
+			db.WriteFile(Path.Combine(di.FullName, "ConsoleApp1.ifc"));
+		}
+	}
+}
